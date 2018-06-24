fb5ccd392de64792ccd9396eb63c0b5eb1f9c208
Levente Kurusa <levex@linux.com>
2018-06-24 05:44:09 +0100

    Add iterating through entries
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+#![no_std]
 extern crate byteorder;
 use byteorder::{ByteOrder, LittleEndian};
 
 /// The MP Floating Pointer Structure as defined in [MPSpec] Section 4.1
 #[repr(C, packed)]
+#[derive(Clone, Copy, Debug)]
 pub struct MPFloatingPointer {
     pub signature: u32,
     pub physical_address_pointer: u32,
@@ -16,6 +18,10 @@ pub struct MPFloatingPointer {
 /// The value should be the ASCII representation of "_MP_" (excluding quotes)
 pub const MPFLOATINGPOINTER_SIGNATURE: [u8; 4] = [95, 77, 80, 95];
 
+/// The signature that should match MPConfigurationTableHeader's signature field
+/// The value should be the ASCII representation of "PCMP" (excluding quotes)
+pub const MPCONFIGURATIONTABLEHEADER_SIGNATURE: [u8; 4] = [80, 67, 77, 80];
+
 impl MPFloatingPointer {
     pub fn verify_checksum(&self) -> bool {
         let mut checksum = 0;
@@ -23,7 +29,7 @@ impl MPFloatingPointer {
 
         /* convert the two 32-bit numbers into an array of bytes */
         LittleEndian::write_u32(&mut buf, self.signature);
-        LittleEndian::write_u32(&mut buf[4..7], self.physical_address_pointer);
+        LittleEndian::write_u32(&mut buf[4..8], self.physical_address_pointer);
 
         /* Add everything up */
         checksum += buf.iter().map(|&b| usize::from(b)).sum::<usize>();
@@ -48,3 +54,110 @@ impl MPFloatingPointer {
         self.verify_checksum() && self.verify_signature()
     }
 }
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Debug)]
+pub struct MPConfigurationTableHeader {
+    pub signature: u32,
+    pub base_table_length: u16,
+    pub spec_rev: u8,
+    pub checksum: u8,
+    pub oem_id: [u8; 8],
+    pub product_id: [u8; 12],
+    pub oem_table_pointer: u32,
+    pub oem_table_size: u16,
+    pub entry_count: u16,
+    pub local_apic_addr: u32,
+    pub extended_table_length: u16,
+    pub extended_table_checksum: u8,
+}
+
+impl MPConfigurationTableHeader {
+
+    pub fn verify_checksum(&self) -> bool {
+        /* FIXME: Actually verify this */
+        true
+    }
+
+    pub fn verify_signature(&self) -> bool {
+        let mut ascii = [0; 4];
+        LittleEndian::write_u32(&mut ascii, self.signature);
+        ascii == MPCONFIGURATIONTABLEHEADER_SIGNATURE
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.verify_checksum() && self.verify_signature()
+    }
+
+    pub fn iter(&self, table_location: usize) -> EntryIterator {
+        EntryIterator {
+            table_location: table_location + 44,
+            total_entries: self.entry_count as usize,
+            entries_sofar: 0,
+            current_offset: 0,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum MPEntryCode {
+    Processor = 0,
+    Bus = 1,
+    IOAPIC = 2,
+    IOInterruptAssignment = 3,
+    LocalInterruptAssignment = 4,
+    Unknown = 5,
+}
+
+impl MPEntryCode {
+    pub fn length(&self) -> usize
+    {
+        match self {
+            MPEntryCode::Processor => 20,
+            MPEntryCode::Bus => 8,
+            MPEntryCode::IOAPIC => 8,
+            MPEntryCode::IOInterruptAssignment => 8,
+            MPEntryCode::LocalInterruptAssignment => 8,
+            MPEntryCode::Unknown =>
+                panic!("Trying to get length of unknown MP entry: {:?}", self),
+        }
+    }
+
+    pub fn from_u8(num: u8) -> Self {
+        match num {
+            0 => MPEntryCode::Processor,
+            1 => MPEntryCode::Bus,
+            2 => MPEntryCode::IOAPIC,
+            3 => MPEntryCode::IOInterruptAssignment,
+            4 => MPEntryCode::LocalInterruptAssignment,
+            _ => MPEntryCode::Unknown,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct EntryIterator {
+    table_location: usize,
+    entries_sofar: usize,
+    total_entries: usize,
+    current_offset: usize,
+}
+
+impl Iterator for EntryIterator {
+    type Item = MPEntryCode;
+
+    fn next(&mut self) -> Option<MPEntryCode> {
+        if self.entries_sofar >= self.total_entries {
+            None
+        } else {
+            let current_addr = self.table_location + self.current_offset;
+            let current_ptr = current_addr as *const u8;
+            let current_code: MPEntryCode = unsafe { MPEntryCode::from_u8(*current_ptr) };
+            self.entries_sofar += 1;
+            self.current_offset += current_code.length();
+            Some(current_code)
+        }
+    }
+}
+
+
